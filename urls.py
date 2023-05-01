2c588c5e31df8db12b6a50c019c0fdde5b9836f4
Saurabh Kumar <116420817+srbh001@users.noreply.github.com>
2023-05-01 18:03:57 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,16 @@
+"""URLs for locations."""
+from django.urls import path
+from locations.views import LocationViewSet, get_shortest_path, nearest_points,checkerrors
+
+urlpatterns = [
+    path('locations', LocationViewSet.as_view({
+        'get': 'list', 'post': 'create'
+    })),
+
+    path('locations/<pk>', LocationViewSet.as_view({
+        'get': 'retrieve', 'put': 'update', 'delete': 'destroy'
+    })),
+    path('shortestpath/', get_shortest_path),
+    path('nearest/', nearest_points),
+    path('check/', checkerrors)
+]
